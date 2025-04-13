96bc6d301f79e29b5eddaa193cf2053dc34af92b
Ahmad Fauzi <fauzitkj3@gmail.com>
2025-04-13 14:03:28 +0700

    feat: get data product & cerate filter

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,45 @@
 import { Pencil, Plus, Settings, Trash2 } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { LoadingTable } from "@/components";
+import api from "@/utils/api";
+import useSWR, { useSWRConfig } from "swr";
 
 export function Products() {
+  const [name, setName] = useState("");
+  const [categoryId, setCategoryId] = useState("");
+  const [page, setPage] = useState(1);
+  const limit = 15; // Default limit 15
+
+  // Query string berdasarkan filter
+  const query = new URLSearchParams({ page, limit });
+  if (name) query.append("name", name);
+  if (categoryId) query.append("categoryId", categoryId);
+
+  const fetcher = async (url) => {
+    try {
+      const response = await api.get(url);
+      const dataCategory = await api.get("/categories?type=main");
+
+      const data = {
+        dataCategory: dataCategory.data.data,
+        dataProducts: response.data.data,
+        totalData: response.data.totalData,
+        totalPages: response.data.totalPages,
+      };
+
+      console.log(data);
+
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { data, error, isLoading } = useSWR(`/products?${query}`, fetcher);
+
+  if (error) return <p>Error loading data.</p>;
+
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">All Products</h1>
@@ -11,22 +48,23 @@ export function Products() {
           <input
             type="search"
             placeholder="Search by product name"
-            // value={name}
-            // onChange={(e) => setName(e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="input input-sm shadow w-full md:1/5"
           />
           <select
-            // value={opticId}
-            // onChange={(e) => setOpticId(e.target.value)}
+            value={categoryId}
+            onChange={(e) => setCategoryId(e.target.value)}
             className="select select-sm shadow w-full md:1/5"
           >
-            <option value="">Categories</option>
-            {/* {optic.length >= 1 &&
-              optic.map((item, index) => (
+            <option value="">All Categories</option>
+            {!isLoading &&
+              data.dataCategory.map((item, index) => (
                 <option key={index} value={item.id}>
-                  {item.optic_name}
+                  {item.name}
                 </option>
-              ))} */}
+              ))}
+            <option value="null">Uncategorized</option>
           </select>
           <select
             // value={opticId}
@@ -50,15 +88,14 @@ export function Products() {
         </div>
       </div>
       <div className="overflow-x-auto card bg-white shadow-md mt-4 p-2 pb-6">
-        <table className="table table-xs w-full min-w-[768px] table-auto">
+        <table className="table table-xs w-full min-w-[640px] table-auto">
           {/* Head */}
           <thead>
             <tr>
               {[
                 "Product Name",
                 "Category",
-                "@Price",
-                "Stock",
+                "Base Price",
                 "Status",
                 <Settings className="h-4 w-4" />,
               ].map((el) => (
@@ -78,147 +115,58 @@ export function Products() {
           </thead>
           {/* Body */}
           <tbody>
-            <tr>
-              <td className="border-b border-gray-200">
-                <p className="text-xs font-semibold capitalize">Nama Produk</p>
-              </td>
-              <td className="border-b border-gray-200">
-                <p className="text-xs font-semibold capitalize">Category</p>
-                <p className="text-xs text-gray-500 font-light">sub, sub</p>
-              </td>
-              <td className="border-b border-gray-200">Rp 17.000</td>
-              <td className="border-b border-gray-200">2000</td>
-              <td className="border-b border-gray-200">
-                <label className="fieldset-label">
-                  <input
-                    type="checkbox"
-                    className="toggle toggle-xs toggle-primary"
-                  />
-                  Active
-                </label>
-              </td>
-              <td className="border-b border-gray-200">
-                <Link
-                  className="btn btn-xs btn-ghost btn-circle text-success tooltip mr-2"
-                  data-tip="Edit"
-                >
-                  <Pencil className="h-4 w-4" />
-                </Link>
-                <Link
-                  className="btn btn-xs btn-ghost btn-circle text-error tooltip"
-                  data-tip="Delete"
-                >
-                  <Trash2 className="h-4 w-4" />
-                </Link>
-              </td>
-            </tr>
-            {/* {!isLoadingPatient ? (
-              dataPatient.data.map(
-                (
-                  {
-                    id,
-                    name,
-                    address,
-                    phone_number,
-                    date_of_birth,
-                    gender,
-                    optic,
-                    createdAt,
-                    medicalconditions,
-                  },
-                  index
-                ) => (
+            {isLoading ? (
+              <LoadingTable row="10" colspan="5" />
+            ) : (
+              data.dataProducts.map(
+                ({ id, name, categories, base_price, status }, index) => (
                   <tr key={index}>
                     <td className="border-b border-gray-200">
-                      {(page - 1) * limit + index + 1}
+                      <p className="text-xs font-semibold capitalize">{name}</p>
                     </td>
                     <td className="border-b border-gray-200">
                       <p className="text-xs font-semibold capitalize">
-                        {name.toLowerCase()}
+                        {categories?.name || "Uncategorized"}
                       </p>
-                      <p className="text-xs text-gray-500 font-light">
-                        {phone_number}
-                      </p>
-                    </td>
-                    <td className="border-b border-gray-200 text-center">
-                      <p className="text-xs font-semibold">
-                        {gender == "Perempuan" ? "P" : "L"}
-                      </p>
-                      <p className="text-xs text-gray-500 font-light">
-                        {dayjs().diff(
-                          dayjs(date_of_birth.split("T")[0]),
-                          "year"
-                        )}{" "}
-                        Thn
-                      </p>
+                      <p className="text-xs text-gray-500 font-light">&nbsp;</p>
                     </td>
                     <td className="border-b border-gray-200">
-                      <p className="text-xs text-gray-700 font-semibold">
-                        {address}
-                      </p>
+                      Rp {base_price}
                     </td>
                     <td className="border-b border-gray-200">
-                      <ul className="list-inside list-disc">
-                        {medicalconditions &&
-                          medicalconditions.map(({ name }, index2) => (
-                            <li key={index2}>{name}</li>
-                          ))}
-                      </ul>
+                      <label className="fieldset-label">
+                        <input
+                          type="checkbox"
+                          className="toggle toggle-xs toggle-primary"
+                          checked={status === 1}
+                          onChange={() => console.log(id)}
+                        />
+                        {status === 1 ? "Active" : "Inactive"}
+                      </label>
                     </td>
                     <td className="border-b border-gray-200">
-                      <p className="text-xs font-semibold text-gray-700">
-                        {optic.optic_name}
-                      </p>
-                      <p className="text-xs text-gray-500 font-light">
-                        {dayjs(createdAt)
-                          .tz("Asia/Jakarta")
-                          .format("DD-MM-YYYY")}
-                      </p>
-                    </td>
-                    <td className="w-28 flex justify-end">
                       <Link
-                        to="/medical-record/add-medical-record"
-                        state={{ patientId: id, prevPage: location.pathname }}
-                        className="btn btn-xs btn-ghost btn-circle text-neutral tooltip"
-                        data-tip="Add Medical Record"
-                      >
-                        <FilePlus className="h-4 w-4" />
-                      </Link>
-                      <Link
-                        to={`/medical-record/patients/${id}`}
-                        className="btn btn-xs btn-ghost btn-circle text-info tooltip"
-                        data-tip="Detail"
-                      >
-                        <Info className="h-4 w-4" />
-                      </Link>
-                      <Link
-                        to={`/medical-record/edit-patient-data/${id}`}
-                        className="btn btn-xs btn-ghost btn-circle text-success tooltip"
+                        className="btn btn-xs btn-ghost btn-circle text-success tooltip mr-2"
                         data-tip="Edit"
                       >
                         <Pencil className="h-4 w-4" />
                       </Link>
-                      {userRole == "admin" && (
-                        <button
-                          className="btn btn-xs btn-ghost btn-circle text-error tooltip"
-                          data-tip="Delete"
-                          onClick={() => handleDelete(id)}
-                        >
-                          <Trash2 className="h-4 w-4" />
-                        </button>
-                      )}
+                      <Link
+                        className="btn btn-xs btn-ghost btn-circle text-error tooltip"
+                        data-tip="Delete"
+                      >
+                        <Trash2 className="h-4 w-4" />
+                      </Link>
                     </td>
                   </tr>
                 )
               )
-            ) : (
-              <LoadingTable row="10" colspan="7" />
-            )} */}
+            )}
           </tbody>
         </table>
       </div>
       {/* Pagination */}
-      {/* <div className="flex flex-col justify-center items-center mt-4">
+      <div className="flex flex-col justify-center items-center mt-4">
         <div className="join">
           <button
             className="join-item btn btn-sm bg-white"
@@ -228,20 +176,20 @@ export function Products() {
             « Prev
           </button>
           <button className="join-item btn btn-sm bg-white font-normal">
-            Page {page} of {!isLoadingPatient && dataPatient.totalPages}
+            Page {page} of {!isLoading && data.totalPages}
           </button>
           <button
             className="join-item btn btn-sm bg-white"
-            disabled={page >= (!isLoadingPatient && dataPatient.totalPages)}
+            disabled={page >= (!isLoading && data.totalPages)}
             onClick={() => setPage(page + 1)}
           >
             Next »
           </button>
         </div>
         <div className="text-xs mt-4">
-          Total Data: {!isLoadingPatient && dataPatient.totalData}
+          Total Data: {!isLoading && data.totalData}
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
